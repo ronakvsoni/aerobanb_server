7c1d27b641ec9ed494820ea9f6b221bc205e2f62
github username <github email address>
2020-10-19 14:37:07 -0700

    working file uploads

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -68,17 +68,17 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  # process resize_to_fit: [400, 400]
-  # # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  process resize_to_fit: [400, 400]
+  # Create different versions of your uploaded files:
+  version :thumb do
+    process resize_to_fit: [50, 50]
 
-  # end
+  end
 
-  # version :demo do
-  #   process resize_to_fit: [200, 200]
-  #   process :round
-  # end
+  version :demo do
+    process resize_to_fit: [200, 200]
+    process :round
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
